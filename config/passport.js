33fbfed02c1b1e10168792507961c9c475395dad
Cytens <businesstopman@gmail.com>
2025-02-23 14:09:04 +0000

    Fixed Google OAuth redirect for production

--- config/passport.js
+++ config/passport.js
@@ -41,7 +41,9 @@ module.exports = function (passport) {
       {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: "http://localhost:3000/auth/google/callback",
+        callbackURL: process.env.NODE_ENV === "production"
+        ? "https://hanect-service.onrender.com/auth/google/callback"
+        : "http://localhost:3000/auth/google/callback",
         passReqToCallback: true,
       },
       async (accessToken, refreshToken, profile, done) => {
